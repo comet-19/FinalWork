6267b2acfb9f6eac7b03a29581925f1923ecd593
comet-19 <tomomizu7773@icloud.com>
2023-10-20 16:01:23 +0900

    環境構築

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import TODOList from "./TODOList";
-import Header from "./Header";
-import React from "react";
 
 function App() {
 	return (
-		<div>
-			<Header />
-			<TODOList />
-		</div>
+		<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
+			React-v3
+		</header>
 	);
 }
 
