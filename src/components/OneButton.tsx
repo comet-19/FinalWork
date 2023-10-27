789bbfc6d9204d8ba5a8b7dab09407f5f18eb1df
comet-19 <tomomizu7773@icloud.com>
2023-10-27 12:03:32 +0900

    ボタンを押したら都道府県名のチェックボックスが出現するようにした

---
+++ src/components/OneButton.tsx
@@ -0,0 +1,10 @@
+
+function OneButton (props:any) {
+    return (
+        <div>
+            <input type="checkbox" name="" id="" /><span>{props.prename}</span>
+        </div>
+    )
+}
+
+export default OneButton;
--- src/components/PreButton.tsx
+++ src/components/PreButton.tsx
@@ -3,17 +3,22 @@ import { LineChart, XAxis, YAxis, CartesianGrid, Line } from "recharts";
 import axios from "axios";
 import { get } from "http";
 import { isConstructorDeclaration } from "typescript";
+import OneButton from "./OneButton";
+
 
 function PreButton() {
 
-    const[PreDatas, setPreData] = useState([1,2]);
+    let Predatas;
+    const [PreNamedatas, setPreNamedatas] = useState<string[]>([]);
 
 
     function AllPre() {
         axios.get("https://opendata.resas-portal.go.jp/api/v1/prefectures", { headers: { "X-API-KEY": "rMpQU6kyxGmETmpOuebxZzmlESj09itPW5d2fMkW" } })
             .then(function (response) {
                 console.log(response.data.result);
-                setPreData(response.data.result);
+                Predatas = response.data.result;
+                const getPreNameDatas = Predatas.map((item:any) => item["prefName"]);
+                setPreNamedatas(getPreNameDatas);
             })
             .catch(function (error) {
                 console.log("error");
@@ -23,6 +28,16 @@ function PreButton() {
     return (
         <div>
             <button onClick={AllPre}>Go</button>
+
+            <div className="PreCheckBox">
+                {
+                    PreNamedatas.map((PreName, index) =>  {
+                        return (
+                            <OneButton prename={PreName} key={index}/>
+                        )
+                    })
+                }
+            </div>
 
         </div>
     );
